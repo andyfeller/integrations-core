e67c1cdbb05eae790a7f0b41e839a1b3503bb5a4
Greg Meyer <greg.meyer@datadoghq.com>
2018-02-07 14:29:01 -0500

    istio

--- istio/datadog_checks/istio/__init__.py
+++ istio/datadog_checks/istio/__init__.py
@@ -1,7 +1,11 @@
-from . import istio
+from .istio import Istio
+from .__about__ import __version__
 
 Istio = istio.Istio
 
 __version__ = "1.0.0"
 
-__all__ = ['istio']
+__all__ = [
+    'Istio'
+    '__version__'
+]
